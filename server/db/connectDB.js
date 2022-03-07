70a8a7069ecc6bc19ebcf2455a26e45d6f93c569
HardJoe <jonathanamadeus95@gmail.com>
2022-03-07 10:41:52 +0700

    Refactor index and connectDB

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,6 +1,15 @@
 require('dotenv').config({ path: './.env' });
+
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message);
+  });
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config({ path: './.env' });
+
 const app = require('./app');
 const http = require('http');
-var server = http.createServer(app);
-require('dotenv').config({ path: './.env' });
 const mongoose = require('./db/connectDB');
+
+const server = http.createServer(app);
+
 const port = process.env.PORT;
 server.listen(port, function () {
   console.log('Server running at port ' + port);
