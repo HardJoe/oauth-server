13a81e6755058d5e1b6077729ec381ae82708c13
HardJoe <jonathanamadeus95@gmail.com>
2022-03-07 12:23:54 +0700

    Delete /api from routes

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,49 +1,20 @@
 const express = require('express');
 const R = require('ramda');
 
-const { verifyAuthToken } = require('../middlewares/authenticate');
-const projectMiddleware = require('../middlewares/projectMiddleware');
 const User = require('../models/user');
 
 const router = express.Router();
 
 router.route('/register').post(function (req, res) {
-  var body = R.pick(
-    ['userId', 'name', 'npm', 'email', 'phone', 'password'],
-    req.body
-  );
+  var body = R.pick(['username', 'full_name', 'npm', 'password'], req.body);
+  body.fullName = body.full_name;
+  delete body.full_name;
+  console.log('body', body);
   var user = new User(body);
   user
     .save()
     .then(function () {
-      return user.generateAuthToken();
-    })
-    .then(function (token) {
-      res.header('x-auth', token).send(R.pick(['name'], user));
-    })
-    .catch(function (e) {
-      console.log(e);
-      res.status(400).send({ code: 400, message: e });
-    });
-});
-
-router.route('/login').post(async function (req, res) {
-  var body = R.pick(['email', 'password'], req.body);
-  try {
-    var user = await User.findByCredentials(body.email, body.password);
-    var token = await user.generateAuthToken();
-    console.log('token', token);
-    res.header('x-auth', token).send(R.pick(['name'], user));
-  } catch (e) {
-    console.log(e);
-    res.status(400).send({ code: 400, message: e });
-  }
-});
-router.route('/logout').delete(verifyAuthToken, function (req, res) {
-  req.user
-    .removeToken(req.token)
-    .then(function () {
-      res.send({ message: 'Logout Successfull' });
+      res.status(201).send(R.pick(['username', 'fullName', 'npm'], user));
     })
     .catch(function (e) {
       console.log(e);
