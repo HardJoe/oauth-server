c39d692c8321d1d694dbb4ab0db4d5a6961be56f
HardJoe <jonathanamadeus95@gmail.com>
2022-03-10 11:55:25 +0700

    Remove some functions in user models

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,6 @@ require('dotenv').config({ path: './.env' });
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
 
 var Schema = mongoose.Schema;
 
@@ -27,35 +26,11 @@ var UserSchema = new Schema({
     type: String,
     required: true,
   },
-  created_at: { type: Date, default: Date.now },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
-
-UserSchema.methods.generateAccessToken = function (client_id) {
-  var user = this;
-  const accessToken = crypto.randomBytes(20).toString('hex');
-  user.access_tokens.push({
-    token: accessToken,
-    client_id,
-    expires: new Date(Date.now + 5 * 60000),
-  });
-  const refreshToken = crypto.randomBytes(20).toString('hex');
-  user.access_tokens.push({
-    token: refreshToken,
-    client_id,
-    expires: new Date(Date.now + 30 * 60000),
-  });
-  return user.save().then(function () {
-    return { accessToken, refreshToken };
-  });
-};
-
-UserSchema.statics.findByToken = async function (token) {
-  var User = this;
-  const user = await User.findOne({
-    'access_tokens.token': token,
-  });
-  return { user };
-};
 
 UserSchema.pre('save', function (next) {
   var user = this;
@@ -70,14 +45,5 @@ UserSchema.pre('save', function (next) {
     next();
   }
 });
-
-UserSchema.methods.removeToken = function (token) {
-  var user = this;
-  return user.updateOne({
-    $pull: {
-      access_tokens: { token },
-    },
-  });
-};
 
 module.exports = mongoose.model('User', UserSchema);
