513d3d3568ee66bd2d9e4528a146c75e64e2bb71
HardJoe <jonathanamadeus95@gmail.com>
2022-03-11 13:32:17 +0700

    Add error message to createProject

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -4,24 +4,27 @@ const R = require('ramda');
 const Project = require('../models/project');
 
 const createProject = async function (req, res) {
-  var data = R.pick(['name'], req.body);
+  const body = R.pick(['name'], req.body);
 
-  data.client_id = crypto.randomBytes(16).toString('hex');
+  body.client_id = crypto.randomBytes(16).toString('hex');
 
   const hash = crypto
     .createHmac('sha256', process.env.SECRET)
-    .update(data.client_id)
+    .update(body.client_id)
     .digest('hex');
-  data.client_secret = hash;
+  body.client_secret = hash;
 
-  var project = new Project(data);
+  let project = new Project(body);
   try {
-    var project = await project.save();
+    project = await project.save();
     res
       .status(201)
       .send(R.pick(['name', 'client_id', 'client_secret'], project));
   } catch (e) {
-    res.status(406).send({ code: 406, message: 'invalid name' });
+    res.status(400).send({
+      error: 'invalid request',
+      error_description: e.message,
+    });
   }
 };
 
