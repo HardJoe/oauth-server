51d5edfd09c271390928fc849ef8d4fd0919fc79
HardJoe <jonathanamadeus95@gmail.com>
2022-03-11 14:38:20 +0700

    Add Procfile

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoose = require('./src/db/connectDb');
 
 const server = http.createServer(app);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 server.listen(port, '0.0.0.0', function () {
   console.log('Server running at port ' + port);
 });
