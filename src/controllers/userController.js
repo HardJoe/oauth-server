7339fdac95f31322fc74b3185298da1b5d453c10
HardJoe <jonathanamadeus95@gmail.com>
2022-03-11 13:15:42 +0700

    Add error message to createUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,18 +2,18 @@ const R = require('ramda');
 
 const User = require('../models/user');
 
-const createUser = function (req, res) {
-  var body = R.pick(['username', 'full_name', 'npm', 'password'], req.body);
-  var user = new User(body);
-
-  user
-    .save()
-    .then(function () {
-      res.status(201).send(R.pick(['username', 'full_name', 'npm'], user));
-    })
-    .catch(function (e) {
-      res.status(400).send({ code: 400, message: e });
+const createUser = async function (req, res) {
+  const body = R.pick(['username', 'full_name', 'npm', 'password'], req.body);
+  let user = new User(body);
+  try {
+    user = await user.save();
+    res.status(201).send(R.pick(['username', 'full_name', 'npm'], user));
+  } catch (e) {
+    res.status(400).send({
+      error: 'invalid request',
+      error_description: e.message,
     });
+  }
 };
 
 module.exports = { createUser };
