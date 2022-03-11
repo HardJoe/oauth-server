6ae50e87ee4c36cef5674e8e1277e6cce0f6f2ed
HardJoe <jonathanamadeus95@gmail.com>
2022-03-11 11:54:05 +0700

    Change route names to camelCase

--- src/app.js
+++ src/app.js
@@ -1,27 +1,17 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const userRouter = require('./routes/UserRoutes');
-const projectRouter = require('./routes/ProjectRoutes');
-const OAuthRouter = require('./routes/OAuthRoutes');
+const userRouter = require('./routes/userRoutes');
+const projectRouter = require('./routes/projectRoutes');
+const oAuthRouter = require('./routes/oAuthRoutes');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Expose-Headers', 'x-auth');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, content-type, Accept, x-auth'
-  );
-  next();
-});
 
 app.use('/user', userRouter);
 app.use('/project', projectRouter);
-app.use('/oauth', OAuthRouter);
+app.use('/oauth', oAuthRouter);
 
 module.exports = app;
